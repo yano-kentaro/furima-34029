cd80166cb655c1b1bd8489db37ed290050a80163
yano-kentaro <ynkn0829@gmail.com>
2021-01-26 20:20:56 +0900

    Revert "パスワードの英数混合判定"
    This reverts commit 122e2a992adff0915287b8823acc2dad9516cdc2.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :nickname, presence: true
-  validates :birth_date, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
