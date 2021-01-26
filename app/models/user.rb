0883597a22e9b338933692ec42f675cf3e4f5441
yano-kentaro <ynkn0829@gmail.com>
2021-01-26 20:20:51 +0900

    Revert "名前のバリデーション設定"
    This reverts commit a6fc9220647d511d96c9bc09a6cd4167903892b6.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name, presence: true
+  validates :last_name_kana, presence: true
   validates :nickname, presence: true
   validates :birth_date, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
