2573b8213f8511f9eda534ba9f7ddc35213df909
yano-kentaro <ynkn0829@gmail.com>
2021-01-27 14:47:11 +0900

    コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,15 +2,20 @@ class ItemsController < ApplicationController
   def index
   end
   def new
-    
+    @item = Item.new
   end
   def create
-    
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   private
   def items_params
-    params.require(:items).permit(
-      :name, :explanation, :price, :category_id, :condition_id, :shipping_charge_id, :delivery_date_id, :prefecture_id
+    params.require(:item).permit(
+      :image, :name, :explanation, :price, :category_id, :condition_id, :shipping_charge_id, :delivery_date_id, :prefecture_id
     ).merge(user_id: current_user.id )
   end
 end
