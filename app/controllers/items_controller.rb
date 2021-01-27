0721f0f01ef73793e7f6c59377e8d7d1bbc43180
yano-kentaro <ynkn0829@gmail.com>
2021-01-27 12:21:04 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+  def new
+    
+  end
+  def create
+    
+  end
+  private
+  def items_params
+    params.require(:items).permit(
+      :name, :explanation, :price, :category_id, :condition_id, :shipping_charge_id, :delivery_date_id, :prefecture_id
+    ).merge(user_id: current_user.id )
+  end
 end
