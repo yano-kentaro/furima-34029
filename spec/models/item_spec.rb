4acc70d86a87580780705a7f22736c63eb25e5a6
yano-kentaro <ynkn0829@gmail.com>
2021-01-27 20:17:15 +0900

    ご指摘いただいた箇所の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,47 +6,7 @@ RSpec.describe Item, type: :model do
       @item = FactoryBot.build(:item)
     end
     context '商品出品出来る' do
-      it '商品画像が1枚添付されている' do
-        expect(@item).to be_valid
-      end
-      it '商品名が記入されている' do
-        @item.name = 'test'
-        expect(@item).to be_valid
-      end
-      it '商品説明が記入されている' do
-        @item.explanation = 'test'
-        expect(@item).to be_valid
-      end
-      it 'カテゴリー情報が入力されている' do
-        @item.category_id = 2
-        expect(@item).to be_valid
-      end
-      it '商品の状態が記入されている' do
-        @item.condition_id = 2
-        expect(@item).to be_valid
-      end
-      it '配送料の負担先が記入されている' do
-        @item.shipping_charge_id = 2
-        expect(@item).to be_valid
-      end
-      it '発送元の地域が記入れている' do
-        @item.prefecture_id = 2
-        expect(@item).to be_valid
-      end
-      it '発送までの日数が記入されている' do
-        @item.delivery_date_id = 2
-        expect(@item).to be_valid
-      end
-      it '販売価格が記入されている' do
-        @item.price = 300
-        expect(@item).to be_valid
-      end
-      it '販売価格が300~9,999,999円の間である' do
-        @item.price = 300
-        expect(@item).to be_valid
-      end
-      it '販売価格が半角数字で記入されている' do
-        @item.price = 300
+      it '全ての情報があるときに出品できる' do
         expect(@item).to be_valid
       end
     end
@@ -71,25 +31,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'id = 1を選択すると登録出来ない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
       it '商品の状態が記入されていない' do
         @item.condition_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
-      it '配送料の負担先が記入されている' do
+      it 'id = 1を選択すると登録出来ない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+      it '配送料の負担先が記入されていない' do
         @item.shipping_charge_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
+      end
+      it 'id = 1を選択すると登録出来ない' do
+        @item.shipping_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
       end
       it '発送元の地域が記入されていない' do
         @item.prefecture_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
+      it 'id = 1を選択すると登録出来ない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
       it '発送までの日数が記入されていない' do
         @item.delivery_date_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
+      end
+      it 'id = 1を選択すると登録出来ない' do
+        @item.delivery_date_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery date must be other than 1")
       end
       it '販売価格が記入されていない' do
         @item.price = nil
@@ -110,6 +95,11 @@ RSpec.describe Item, type: :model do
         @item.price = 'abcde'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '販売価格が英数混合だと登録できない' do
+        @item.price = 'abc123'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
