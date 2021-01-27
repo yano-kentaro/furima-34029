40b80b0b4a2999a386da17d530a2cbb0e005dfd7
yano-kentaro <ynkn0829@gmail.com>
2021-01-27 20:36:04 +0900

    ご指摘いただいた箇所の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -101,6 +101,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+      it '販売価格が全角文字だと登録できない' do
+        @item.price = '１２３４５'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
     end
   end
 end
