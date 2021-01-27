5d7137aab4aa99378cc988d43947491163e9cc12
yano-kentaro <ynkn0829@gmail.com>
2021-01-27 20:20:33 +0900

    rubocop

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Item, type: :model do
       it 'id = 1を選択すると登録出来ない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it '商品の状態が記入されていない' do
         @item.condition_id = nil
@@ -44,7 +44,7 @@ RSpec.describe Item, type: :model do
       it 'id = 1を選択すると登録出来ない' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+        expect(@item.errors.full_messages).to include('Condition must be other than 1')
       end
       it '配送料の負担先が記入されていない' do
         @item.shipping_charge_id = nil
@@ -54,7 +54,7 @@ RSpec.describe Item, type: :model do
       it 'id = 1を選択すると登録出来ない' do
         @item.shipping_charge_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipping charge must be other than 1')
       end
       it '発送元の地域が記入されていない' do
         @item.prefecture_id = nil
@@ -64,7 +64,7 @@ RSpec.describe Item, type: :model do
       it 'id = 1を選択すると登録出来ない' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it '発送までの日数が記入されていない' do
         @item.delivery_date_id = nil
@@ -74,7 +74,7 @@ RSpec.describe Item, type: :model do
       it 'id = 1を選択すると登録出来ない' do
         @item.delivery_date_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery date must be other than 1")
+        expect(@item.errors.full_messages).to include('Delivery date must be other than 1')
       end
       it '販売価格が記入されていない' do
         @item.price = nil
@@ -99,7 +99,7 @@ RSpec.describe Item, type: :model do
       it '販売価格が英数混合だと登録できない' do
         @item.price = 'abc123'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
