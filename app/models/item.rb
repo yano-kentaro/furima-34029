4b19e0bb497628027c1f642aede44be8f289106f
yano-kentaro <ynkn0829@gmail.com>
2021-01-27 12:08:09 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_charge_id
+      validates :delivery_date_id
+      validates :prefecture_id
+    end
+  end
+
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charge
