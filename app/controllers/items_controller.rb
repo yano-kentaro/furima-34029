67b2e5a7a21a03e3711f044bf01d6e05db1a5aad
yano-kentaro <ynkn0829@gmail.com>
2021-01-28 14:56:09 +0900

    URL直接入力があった際の遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,6 +47,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless user_signed_in? && current_user.id == @item.user.id
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 end
