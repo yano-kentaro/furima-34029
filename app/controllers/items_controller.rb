d0d394e74f13c0064a501df32d5a8f9f14687ac9
yano-kentaro <ynkn0829@gmail.com>
2021-01-28 15:29:14 +0900

    商品削除機能の実装(コメントアウト）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
-  before_action :set_params, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -33,6 +33,12 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+  
+  # 編集機能をマージするまでコメントアウトしておく
+  # def destroy
+  #   @item.destroy
+  #   redirect_to root_path
+  # end
 
   private
 
