94d1704fbe94a20ca2744ecf256ef17d5ad719c2
yano-kentaro <ynkn0829@gmail.com>
2021-01-29 11:20:18 +0900

    削除失敗した際の処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,11 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
