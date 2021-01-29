6cec1591adeb697803c55ee4e514a4bd801410ac
yano-kentaro <ynkn0829@gmail.com>
2021-01-29 23:51:22 +0900

    Formオブジェクトパターン設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_item
+  
   def index
-    @item = Item.find(params[:item_id])
     @address_order = AddressOrder.new
   end
   def create
     @address_order = AddressOrder.new(address_order_params)
     if @address_order.valid?
+      pay_item
       @address_order.save
       redirect_to root_path
     else
@@ -15,7 +18,30 @@ class OrdersController < ApplicationController
 
   private
 
+  def find_item
+    @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
+    if @item.order != nil
+      redirect_to root_path
+    end
+  end
+
   def address_order_params
-    params.require(:address_order).permit(:postcode, :city, :address_line_1, :address_line_2, :phone_number, :prefecture_id)
+    params.require(:address_order).permit(
+      :postcode, :city, :address_line_1, :address_line_2, :phone_number, :prefecture_id
+    ).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: address_order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
