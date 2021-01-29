dec4377bccf32328736d9714b76264a3b734fd91
yano-kentaro <ynkn0829@gmail.com>
2021-01-29 23:46:58 +0900

    JavaScript記述

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,10 +1,12 @@
 window.addEventListener('load', () => {
   const itemPrice = document.getElementById("item-price")
-itemPrice.addEventListener('input', () => {
+  if(itemPrice != null) {
+  itemPrice.addEventListener('input', () => {
   const itemValue = itemPrice.value
   const addTaxPrice = document.getElementById("add-tax-price")
   const profit = document.getElementById("profit")
   addTaxPrice.innerHTML = itemValue * 0.1
   profit.innerHTML = itemValue * 0.9
 })
+}
 })
